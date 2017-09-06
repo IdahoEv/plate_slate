b7ac093f55c4cfc8ffdcbcb8a0f05c65d94679fc
Evan Dorn <evan@lrdesign.com>
2017-09-06 09:04:25 -0700

    add absinthe dependencies

--- mix.exs
+++ mix.exs
@@ -38,7 +38,11 @@ defmodule PlateSlate.Mixfile do
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:absinthe,         "~> 1.4.0-beta", override: true},      
+      {:absinthe_plug,    "~> 1.4.0-beta", override: true},
+      {:absinthe_phoenix, "~> 1.4.0-beta", override: true},
+      {:absinthe_ecto, ">= 0.0.0"}
     ]
   end
 
